f70518dc4f32e951b8044bc8f23763f6a415231b
Bram van Rijswijk <b.van.rijswijk@socialblue.com>
2021-05-01 14:34:13 +0200

    Makes it responsive for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app" class="w-screen h-screen flex items-center justify-center">
-        <div class="flex items-center justify-center container space-x-12">
-            <img src="https://pbs.twimg.com/profile_images/1387132385431625735/7vyJtFhz_200x200.jpg" alt="" class="rounded-full border-4 border-white">
+        <div class="flex flex-col sm:flex-row items-center justify-center container-xl space-x-0 sm:space-x-12 space-y-10 sm:space-y-0">
+            <img src="./assets/7vyJtFhz_160x160.jpeg" alt="" class="rounded-full border-4 border-white self-start">
             <section class="flex flex-col space-y-3">
                 <h1 class="text-3xl uppercase text-white font-black">Bram van Rijswijk</h1>
                 <section>
