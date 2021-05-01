bd3c72802730594eddf4d84fe2a3a0103f7d67b3
Bram van Rijswijk <b.van.rijswijk@socialblue.com>
2021-05-01 14:46:49 +0200

    Fixes style for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
     <div id="app" class="w-screen h-screen flex items-center justify-center">
         <div class="flex flex-col sm:flex-row items-center justify-center container-xl space-x-0 sm:space-x-12 space-y-10 sm:space-y-0">
-            <img src="./assets/7vyJtFhz_160x160.jpeg" alt="" class="rounded-full border-4 border-white self-start">
-            <section class="flex flex-col space-y-3">
-                <h1 class="text-3xl uppercase text-white font-black">Bram van Rijswijk</h1>
+
+            <img src="https://pbs.twimg.com/profile_images/1387132385431625735/7vyJtFhz_400x400.jpg" alt="" class="block sm:hidden rounded-full border-4 border-white self-start" style="width: 50%;">
+            <img src="./assets/7vyJtFhz_160x160.jpeg" alt="" class="hidden sm:block rounded-full border-4 border-white self-start">
+
+            <section class="flex flex-col space-y-3 self-start sm:self-auto">
+                <h1 class="text-2xl sm:text-3xl uppercase text-white font-black">Bram van Rijswijk</h1>
                 <section>
-                    <p class="text-white font-medium tracking-wider">Back-end Developer @ <a href="https://socialblue.com" class="font-extrabold hover:underline">Social Blue</a></p>
-                    <p class="text-white font-medium tracking-wider">Creator of <a href="https://watisonsbudget.nl" class="font-extrabold hover:underline">Watisonsbudget</a></p>
+                    <p class="text-sm sm:text-base text-white font-medium tracking-wider">Back-end Developer @ <a href="https://socialblue.com" class="font-extrabold hover:underline">Social Blue</a></p>
+                    <p class="text-sm sm:text-base text-white font-medium tracking-wider">Creator of <a href="https://watisonsbudget.nl" class="font-extrabold hover:underline">Watisonsbudget</a></p>
                 </section>
             </section>
         </div>
